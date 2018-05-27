113aecb80b2d578b30d413af688e5e4649a2b812
anyi333 <Wangjh397@163.com>
2018-05-27 22:24:08 +0800

    title左侧图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,16 @@
 # 主页模块
 from . import index_blue
-from flask import render_template
+from flask import render_template,current_app
 
 @index_blue.route('/')
 def index():
     '''主页'''
 
+
+    # 渲染主页
     return render_template('news/index.html')
+
+@index_blue.route('/favicon',methods=['GET'])
+def favicon():
+    '''title左侧图标'''
+    return current_app.send_static_file('news/favicon.ico')
