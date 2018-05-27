817c251fae704485d474f571ff290484c3c52985
anyi333 <Wangjh397@163.com>
2018-05-27 22:00:19 +0800

    渲染主页

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,9 @@
 # 主页模块
 from . import index_blue
-from info import redis_store
+from flask import render_template
 
 @index_blue.route('/')
 def index():
     '''主页'''
 
-    # 操作redis
-    redis_store.set('name','wjh')
-    return 'index'
+    return render_template('news/index.html')
