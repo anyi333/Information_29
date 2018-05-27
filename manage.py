5c5b6ec46d67bb0b8faa48cdf16498877f73ec37
anyi333 <Wangjh397@163.com>
2018-05-27 19:53:48 +0800

    项目第二天

---
+++ manage.py
@@ -0,0 +1,25 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+# from info import app, db
+from info import create_app, db
+
+
+app = create_app('dev')
+
+# 创建脚本管理器对象
+manager = Manager(app)
+# 让迁移和app和数据库建立管理
+Migrate(app, db)
+# 将数据库迁移的脚本添加到manager
+manager.add_command('mysql', MigrateCommand)
+
+
+@app.route('/')
+def index():
+
+    return 'index'
+
+
+if __name__ == '__main__':
+
+    manager.run()
