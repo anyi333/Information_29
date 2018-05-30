2edcc15b57814c16cc8eb00bb60a96fdfdbf1d7a
anyi333 <Wangjh397@163.com>
2018-05-30 13:05:17 +0800

    分页显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,52 @@
 # 主页模块
 from . import index_blue
-from flask import render_template,current_app,session
+from flask import render_template,current_app,session,request,jsonify
 from info.models import User,News
 from info import constants,response_code
 
+
+@index_blue.route('/news_list')
+def index_news_list():
+    '''提供主页新闻列表数据'''
+    # 1.接收参数(分类id,要看几页,每页几条数据)
+    cid = request.args.get('cid','1')
+    page = request.args.get('page','1')
+    per_page = request.args.get('per_page','10')
+
+    # 2.校验参数(判断以上参数是否为数字)
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=response_code.RET.PARAMERR,errmsg='参数错误')
+
+    # 3.根据参数查询用户想看的新闻列表数据
+    if cid == 1:
+        # 从所有新闻中,根据时间倒序,每页取出10条数据
+        paginate = News.query.order_by(News.create_time.desc()).paginate(page,per_page,False)
+    else:
+        # 从指定分类中,查询新闻,根据时间倒序,每页取出10条数据
+        paginate = News.query.filter(News.category_id == cid).order_by(News.create_time.desc()).paginate(page,per_page,False)
+
+    # 4.构造响应的新闻列表数据
+    # 取出当前页的所有的模型对象
+    news_list = paginate.items
+    # 读取分页的总页数,将来在主页新闻李表上拉刷新时使用
+    total_page = paginate.pages
+    # 读取当前是第几页
+    current_page = paginate.page
+
+    # 构造响应给客户端的数据
+    data = {
+        'news_list':news_list,
+        'total_page':total_page,
+        'current_page':current_page
+    }
+
+    # 5.响应新闻列表数据
+    return jsonify(errno=response_code.RET.OK,errmsg='OK',data=data)
 
 @index_blue.route('/')
 def index():
