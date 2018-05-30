e6349eddb95796aacda6c226bb14940276673d81
anyi333 <Wangjh397@163.com>
2018-05-30 11:20:53 +0800

    自定义过滤器实现点击排行特殊序号展示

--- info/__init__.py
+++ info/__init__.py
@@ -7,6 +7,7 @@ from flask_session import Session
 from config import configs
 import logging
 from logging.handlers import RotatingFileHandler
+from info.utils.comment import do_rank
 
 
 def setup_log(level):
@@ -61,6 +62,10 @@ def create_app(config_name):
         # 将csrf_token的值写入到cookie
         response.set_cookie('csrf_token',csrf_token)
         return response
+
+    # 将自定义的过滤器函数,添加到app的过滤器列表中
+    # rank:在模板中使用的别名
+    app.add_template_filter(do_rank,'rank')
 
     # 指定session数据存储在后端的位置
     Session(app)
